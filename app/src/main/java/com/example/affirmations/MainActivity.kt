49f2181ed06cf888efe7c349d1a813642145fe51
shogo.takasaki <stakasaki707@gmail.com>
2024-09-28 19:28:51 +0900

    create AffirmationCardPreview

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.affirmations.data.Affirmation
 import com.example.affirmations.ui.theme.AffirmationsTheme
@@ -82,4 +83,9 @@ fun AffirmationCard(
             )
         }
     }
+}
+
+@Preview
+@Composable fun AffirmationCardPreview() {
+    AffirmationCard(Affirmation(R.string.affirmation1, R.drawable.image1))
 }
